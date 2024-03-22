25f423d487c9b5e5edef06d54df1c4bc873ad87a
SharoonRafeek <sharoonrafeek@gmail.com>
2024-03-22 15:12:10 +0530

    initail

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { redirect, useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // const router = useRouter();
+  // router.push("quassoliberum.com/ca");
+  redirect("https://quassoliberum.com/ca");
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
